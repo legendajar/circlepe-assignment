9e00eb0fb3c7a705c8c855df1a5395c220e2e7f3
legendajar <harshsolanki989@gmail.com>
2024-09-13 16:15:13 +0530

    Refactor cart model and controller to support space stations and products

---
+++ server/controllers/cart.controller.js
@@ -0,0 +1,145 @@
+import cartModel from "../models/cart.model";
+
+export const addCart = async (req, res) => {
+    try {
+        const { user_id, productList, price } = req.body
+
+        const requiredDetails = { user_id, productList, price }
+        for (const [field, value] of Object.entries(requiredDetails)) {
+            if (!value) {
+                return res.status(400).json({
+                    success: false,
+                    message: `${field.charAt(0).toUpperCase() + field.slice(1)} is required`
+                });
+            }
+        }
+
+        const newCart = new cartModel({
+            user_id: user_id,
+            products: productList,
+            price: price
+        })
+
+        const savedCart = await newCart.save();
+        return res.status(201).json({
+            success: true,
+            message: "Cart added successfully",
+            data: savedCart
+        })
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
+    }
+}
+
+export const viewCart = async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!id) {
+            return res.status(404).json({
+                success: false,
+                message: "Invalid Id"
+            })
+        }
+
+        const cart = await cartModel.findOne({ spaceStation_id: id });
+        if (!cart) {
+            return res.status(404).json({
+                success: false,
+                message: "Cart Not Found"
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            data: cart
+        })
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+        })
+    }
+}
+
+export const updateCart = async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!id) {
+            return res.status(404).json({
+                success: false,
+                message: "Invalid Id"
+            })
+        }
+
+        const cart = await cartModel.findOne({ spaceStation_id: id });
+        if (!cart) {
+            return res.status(404).json({
+                success: false,
+                message: "Cart Not Found"
+            })
+        }
+
+        const updatedData = {
+            productList: productList,
+            price: price
+        }
+
+        const updatedCart = await cartModel.findByIdAndUpdate(id, updatedData, { new: true });
+        if (!updatedCart) {
+            return res.status(404).json({
+                success: false,
+                message: "Something not good"
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Cart updated successfully",
+            data: updatedCart
+        })
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+        })
+    }
+}
+
+
+export const deleteCart = async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!id) {
+            return res.status(404).json({
+                success: false,
+                message: "Invalid Id"
+            })
+        }
+
+        const cart = await cartModel.findOne({ spaceStation_id: id });
+        if (!cart) {
+            return res.status(404).json({
+                success: false,
+                message: "Cart Not Found"
+            })
+        }
+        const deletedCart = await cartModel.findByIdAndDelete(id);
+        if (!deletedCart) {
+            return res.status(404).json({
+                success: false,
+                message: "Something not good"
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Cart deleted successfully",
+            data: deletedCart
+        })
+    } catch (err) {
+        console.log(err);
+    }
+}
--- server/models/cart.model.js
+++ server/models/cart.model.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose'
 
 const cartSchema = new mongoose.Schema({
-    user_id: { type: String, required: true },
-    product_id: { type: String, required: true },
+    spaceStation_id: { type: mongoose.Schema.Types.ObjectId, ref:'SpaceStation', required: true },
+    products: [{
+        product_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+        quantity: { type: Number, required: true },
+    }],
     quantity: { type: Number, required: true },
     price: { type: Number, required: true },
 })
