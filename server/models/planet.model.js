732f09d59b7c0246bf382d26383cc82bc6770dbe
legendajar <harshsolanki989@gmail.com>
2024-09-13 11:20:45 +0530

    Add planet and space station models

---
+++ server/models/planet.model.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const planetSchema = new mongoose.Schema({
+    name: {},
+    products: [{
+        id: {type: String, required: true},
+        name: {type: String, required: true},
+        price: {type: Number, required: true},
+        quantity: {type: Number, required: true},
+        image: {type: String, required: true},
+        description: {type: String, required: true},
+        category: {type: String, required: true},
+        ratings: [{
+            userId: {type: String, required: true},
+            rating: {type: Number, required: true},
+            comment: {type: String, required: true}
+        }]
+    }],
+    image: { type: String },
+    mobile: { type: Number, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    last_login: { type: Date, default: Date.now()},
+    last_IP_Address: { type: Number },
+    reset_password: {type: Number},
+    reset_password_time: {type: Date},
+}, {timestamps: true});
+
+const planetModel = mongoose.model("Planet", planetSchema);
+
+export default planetModel
