c2f204884ebbb09f995969206b117d9edb2c8714
legendajar <harshsolanki989@gmail.com>
2024-09-16 03:32:43 +0530

    Refactor API endpoints to use production URLs

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://circlepe-assignment-frontend.vercel.app']
 const corsOptions = {
     origin: (origin, callback) => {
         if (allowedOrigins.includes(origin) || !origin) {
