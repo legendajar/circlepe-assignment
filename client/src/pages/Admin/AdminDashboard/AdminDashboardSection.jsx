82120f246e9eb2210741a8baa936ed61f694d633
legendajar <harshsolanki9898@gmail.com>
2024-10-15 17:23:09 +0530

    Refactor AdminDashboardSection component to display space station and planet information

--- client/src/pages/Admin/AdminDashboard/AdminDashboardSection.jsx
+++ client/src/pages/Admin/AdminDashboard/AdminDashboardSection.jsx
@@ -12,17 +12,26 @@ import {
 } from 'chart.js';
 import AdminDashboardCard from '@/components/Admin/AdminDashboardCards.jsx/AdminDashboardCard';
 import AdminDashboardChartData from '@/components/Admin/AdminDashboardChartData./AdminDashboardChartData';
+import { LucideBuilding2, Users2 } from 'lucide-react';
+import useGetAllSpaceStation from '@/hooks/useGetAllSpaceStation';
+import useGetAllPlanets from '@/hooks/useGetAllPlanets';
+import { useSelector } from 'react-redux';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const AdminDashboardSection = () => {
+  useGetAllSpaceStation();
+  useGetAllPlanets();
+
+  const spaceStations = useSelector(store => store.spaceStation.spaceStationList)
+  const planets = useSelector(store => store.planet.planetList)
   return (
     <div className="flex flex-col gap-8 p-6 bg-gray-100 min-h-screen">
       {/* Dashboard Cards Section */}
       <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-        <AdminDashboardCard />
-        <AdminDashboardCard />
-        <AdminDashboardCard />
+        <AdminDashboardCard title='Space Stations' iconElement={<Users2 className="w-6 h-6 text-indigo-500" />} subTitle='Total Space Stations' count={spaceStations?.length || 0} />
+        <AdminDashboardCard title='Planets' iconElement={<LucideBuilding2 className="w-6 h-6 text-indigo-500" />} subTitle='Total Planets' count={planets?.length || 0} />
+        <AdminDashboardCard title='Space Stations' iconElement={<Users2 className="w-6 h-6 text-indigo-500" />} subTitle='Total Space Stations' count={spaceStations?.length || 0} />
       </div>
 
       {/* Chart Section */}
